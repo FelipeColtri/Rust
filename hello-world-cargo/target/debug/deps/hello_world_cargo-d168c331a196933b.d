6181d055c1075454781c87235fed3f622942b64c
FelipeColtri <felipealeixocoltri@gmail.com>
2023-05-22 23:57:27 -0300

    everthing starts with hello world

---
+++ hello-world-cargo/target/debug/deps/hello_world_cargo-d168c331a196933b.d
@@ -0,0 +1,5 @@
+/home/lite/Documents/Rust/hello-world-cargo/target/debug/deps/hello_world_cargo-d168c331a196933b: src/main.rs
+
+/home/lite/Documents/Rust/hello-world-cargo/target/debug/deps/hello_world_cargo-d168c331a196933b.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world-cargo/target/debug/hello-world-cargo.d
@@ -0,0 +1,1 @@
+/home/lite/Documents/Rust/hello-world-cargo/target/debug/hello-world-cargo: /home/lite/Documents/Rust/hello-world-cargo/src/main.rs
