96635af03eaff88d4caf3c4edf479e443dc64e26
FelipeColtri <felipealeixocoltri@gmail.com>
2023-05-23 00:14:07 -0300

    using cargo

---
+++ cargo-hello-world/target/debug/cargo-hello-world.d
@@ -0,0 +1,1 @@
+/home/lite/Documents/Rust/cargo-hello-world/target/debug/cargo-hello-world: /home/lite/Documents/Rust/cargo-hello-world/src/main.rs
---
+++ cargo-hello-world/target/debug/deps/cargo_hello_world-d3990417678797cd.d
@@ -0,0 +1,5 @@
+/home/lite/Documents/Rust/cargo-hello-world/target/debug/deps/cargo_hello_world-d3990417678797cd: src/main.rs
+
+/home/lite/Documents/Rust/cargo-hello-world/target/debug/deps/cargo_hello_world-d3990417678797cd.d: src/main.rs
+
+src/main.rs:
